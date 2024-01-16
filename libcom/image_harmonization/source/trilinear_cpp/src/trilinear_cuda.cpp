9c475c8953cdd04dc99798d4d733e9dcc96a86f0
Udon <udoooon0727@gmail.com>
2024-01-16 15:26:11 +0900

    Update trilinear_cuda.cpp

--- libcom/image_harmonization/source/trilinear_cpp/src/trilinear_cuda.cpp
+++ libcom/image_harmonization/source/trilinear_cpp/src/trilinear_cuda.cpp
@@ -1,6 +1,7 @@
 #include "trilinear_kernel.h"
 #include <torch/extension.h>
-#include <THC/THC.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAEvent.h>
 
 int trilinear_forward_cuda(torch::Tensor lut, torch::Tensor image, torch::Tensor output,
                            int lut_dim, int shift, float binsize, int width, int height, int batch)
@@ -9,8 +10,9 @@ int trilinear_forward_cuda(torch::Tensor lut, torch::Tensor image, torch::Tensor
     float * lut_flat = lut.data<float>();
     float * image_flat = image.data<float>();
     float * output_flat = output.data<float>();
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-    TriLinearForwardLaucher(lut_flat, image_flat, output_flat, lut_dim, shift, binsize, width, height, batch, at::cuda::getCurrentCUDAStream());
+    TriLinearForwardLaucher(lut_flat, image_flat, output_flat, lut_dim, shift, binsize, width, height, batch, stream);
 
     return 1;
 }
@@ -22,14 +24,14 @@ int trilinear_backward_cuda(torch::Tensor image, torch::Tensor image_grad, torch
     float * image_grad_flat = image_grad.data<float>();
     float * image_flat = image.data<float>();
     float * lut_grad_flat = lut_grad.data<float>();
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-    TriLinearBackwardLaucher(image_flat, image_grad_flat, lut_grad_flat, lut_dim, shift, binsize, width, height, batch, at::cuda::getCurrentCUDAStream());
+    TriLinearBackwardLaucher(image_flat, image_grad_flat, lut_grad_flat, lut_dim, shift, binsize, width, height, batch, stream);
 
     return 1;
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("forward", &trilinear_forward_cuda, "Trilinear forward");
-  m.def("backward", &trilinear_backward_cuda, "Trilinear backward");
+  m.def("trilinear_forward_cuda", &trilinear_forward_cuda, "Trilinear forward");
+  m.def("trilinear_backward_cuda", &trilinear_backward_cuda, "Trilinear backward");
 }
-
