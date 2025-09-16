48855b1f3e033c6bb60420cb30ede30188d1e726
Yujie Zhou <453252496@qq.com>
2025-09-16 12:50:17 +0800

    Update model_download.py

--- libcom/utils/model_download.py
+++ libcom/utils/model_download.py
@@ -43,8 +43,6 @@ def download_file_from_network(file_name, save_dir):
         from huggingface_hub import hf_hub_download
         file_path = hf_hub_download(repo_id=hf_repo, filename=file_name, cache_dir=save_dir)
     except Exception as e:
-        raise RuntimeError("huggingface download error, we will upload the weight to modelscope soon") from e
-    
         from modelscope.hub.file_download import model_file_download
         file_path = model_file_download(model_id=ms_repo, 
                                         file_path=file_name, 
